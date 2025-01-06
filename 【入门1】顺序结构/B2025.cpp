f0499360541c8862b07af569a2c0ee489acfecc6
zhoujiaqi <2411351557@qq.com>
2025-01-06 11:50:29 +0800

    add a directory

---
+++ 【入门1】顺序结构/B2025.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+
+using namespace std;
+int main() {
+    printf( "  *\n"
+            " ***\n"
+            "*****\n"
+            " ***\n"
+            "  *\n" );
+    return 0;
+}
