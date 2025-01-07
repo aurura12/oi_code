fc92baaae6c045d11aaac78c7dc567ddd9d254ac
zhoujiaqi <2411351557@qq.com>
2025-01-07 15:25:18 +0800

    P5703

---
+++ 【入门1】顺序结构/P5703.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num_apple, num_people;
+    int sum = 0;
+    cin >> num_apple >> num_people;
+    sum = num_apple * num_people;
+    cout << sum << endl;
+    return 0;
+}
