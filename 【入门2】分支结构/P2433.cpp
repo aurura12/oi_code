b1cb3fdb0906c15dd314193666a9bd345089a486
zhoujiaqi <2411351557@qq.com>
2025-01-07 22:26:59 +0800

    P2433

---
+++ 【入门2】分支结构/P2433.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+#define pi 3.141593
+
+int main() {
+    int t;
+    cin >> t;
+    if ( t == 1 ) {
+        printf( "I love Luogu!" );
+    } else if ( t == 2 ) {
+        printf( "6 4" );
+    } else if ( t == 3 ) {
+        printf( "3\n12\n2" );
+    } else if ( t == 4 ) {
+        printf( "166.667" );
+    } else if ( t == 5 ) {
+        printf( "15" );
+    } else if ( t == 6 ) {
+        printf( "10.8167" );
+    } else if ( t == 7 ) {
+        printf( "110\n90\n0" );
+    } else if ( t == 8 ) {
+        printf( "31.4159\n78.5398\n523.599" );
+    } else if ( t == 9 ) {
+        printf( "22" );
+    } else if ( t == 10 ) {
+        printf( "9" );
+    } else if ( t == 11 ) {
+        printf( "%.4f", 1.0 * 100 / 3 );
+    } else if ( t == 12 ) {
+        printf( "13\nR" );
+    } else if ( t == 13 ) {
+        printf( "16" );
+    } else if ( t == 14 ) {
+        printf( "50" );
+    }
+    return 0;
+}
