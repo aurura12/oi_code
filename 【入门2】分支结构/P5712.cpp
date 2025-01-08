7a636c168c455b424a19e5e9409336a96ea571c3
zhoujiaqi <2411351557@qq.com>
2025-01-08 22:00:27 +0800

    P5712

---
+++ 【入门2】分支结构/P5712.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    if ( n == 1 || n == 0 ) {
+        cout << "Today, I ate " << n << " apple." << endl;
+    } else {
+        cout << "Today, I ate " << n << " apples." << endl;
+    }
+}
